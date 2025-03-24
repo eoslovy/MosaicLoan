a67841ebe4c25e9420cfd38a4f913d65d5e48e86
eoslovy <ydg0630@naver.com>
2025-03-24 14:54:20 +0900

    refactor: 폴더명 변경

---
+++ back/member-service/src/main/java/com/mosaic/auth/dto/KakaoMemberResponse.java
@@ -0,0 +1,27 @@
+package com.mosaic.auth.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class KakaoMemberResponse {
+	private Long id;
+
+	@JsonProperty("properties")
+	private Properties properties;
+
+	@Getter
+	@NoArgsConstructor
+	public static class Properties {
+		private String nickname;
+	}
+
+	public String getNickname() {
+		return properties != null && properties.nickname != null
+			? properties.nickname
+			: "카카오_" + id;
+	}
+}
