3d20b0c092ef9d23368a55163ca6c3aa86e62e58
eoslovy <ydg0630@naver.com>
2025-03-24 14:54:39 +0900

    refactor: oauthId string으로 변경

--- back/member-service/src/main/java/com/mosaic/auth/domain/Member.java
+++ back/member-service/src/main/java/com/mosaic/auth/domain/Member.java
@@ -1,39 +1,48 @@
 package com.mosaic.auth.domain;
 
-import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
-import java.time.LocalDateTime;
 
 @Entity
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "member")
 public class Member {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
 
-    @Column(name = "oauth_id", unique = true, nullable = false)
-    private Long oauthId;
+	@Column(name = "oauth_id", unique = true, nullable = false)
+	private String oauthId;
 
-    @Column(name = "name")
-    private String name;
+	@Column(name = "name")
+	private String name;
 
-    @Column(name = "oauth_provider")
-    private String oauthProvider = "KAKAO";
+	@Column(name = "oauth_provider")
+	private String oauthProvider = "KAKAO";
 
-    @CreationTimestamp
-    @Column(name = "created_at", updatable = false)
-    private LocalDateTime createdAt;
+	@CreationTimestamp
+	@Column(name = "created_at", updatable = false)
+	private LocalDateTime createdAt;
 
-    public void setOauthId(Long oauthId) {
-        this.oauthId = oauthId;
-    }
+	public Member(String oauthId, String name) {
+		this.oauthId = oauthId;
+		this.name = name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public static Member createMember(String oauthId, String name) {
+		return new Member(oauthId, name);
+	}
 }
