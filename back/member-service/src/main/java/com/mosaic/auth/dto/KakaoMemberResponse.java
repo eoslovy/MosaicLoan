1509addf73d3368717514ecf30d172c881edb398
eoslovy <ydg0630@naver.com>
2025-03-25 13:35:55 +0900

    refactor: JsonProperty 어노테이션 삭제

--- back/member-service/src/main/java/com/mosaic/auth/dto/KakaoMemberResponse.java
+++ back/member-service/src/main/java/com/mosaic/auth/dto/KakaoMemberResponse.java
@@ -9,8 +9,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class KakaoMemberResponse {
 	private Long id;
-
-	@JsonProperty("properties")
 	private Properties properties;
 
 	@Getter
