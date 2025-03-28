272f3db26db7f9eae543f603c066acde3bdd80b5
eoslovy <ydg0630@naver.com>
2025-03-28 16:51:06 +0900

    refactor: 안쓰는 함수 삭제

--- back/credit-evaluation-service/src/main/java/com/mosaic/credit/evaluation/repository/EvaluationCaseRepository.java
+++ back/credit-evaluation-service/src/main/java/com/mosaic/credit/evaluation/repository/EvaluationCaseRepository.java
@@ -3,8 +3,6 @@ package com.mosaic.credit.evaluation.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 
 import com.mosaic.credit.evaluation.domain.EvaluationCase;
 
@@ -12,9 +10,4 @@ public interface EvaluationCaseRepository extends JpaRepository<EvaluationCase, 
 
 	// 사용 가능한 case_id 하나 가져오기
 	Optional<EvaluationCase> findFirstByIsCheckedFalse();
-
-	// case_id로 is_checked = true로 업데이트
-	@Modifying
-	@Query("update EvaluationCase e set e.isChecked = true where e.caseId = :caseId")
-	void markAsChecked(Integer caseId);
 }
