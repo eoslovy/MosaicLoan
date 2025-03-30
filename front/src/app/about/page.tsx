0fe92700808463f8fad340d0bf5b81a1a045ed52
박유진 <galilee155@naver.com>
2025-03-30 23:54:10 +0900

    feat: add mypage NavTab - 화면 테스트위해 일단 서비스 소개 페이지에 작성중 - SectionTab 재사용

--- front/src/app/about/page.tsx
+++ front/src/app/about/page.tsx
@@ -1,5 +1,35 @@
-const AboutPage = () => {
-  return <h1>여기는 서비스 소개 페이지</h1>;
+'use client';
+
+import React, { useState } from 'react';
+import MyPageSectionTabNav from '@/components/my/MyPageSectionTabNav';
+
+const MyPage = () => {
+  const [activeTabIndex, setActiveTabIndex] = useState(0);
+
+  const handleTabClick = (index: number) => {
+    setActiveTabIndex(index);
+  };
+
+  const renderTabContent = () => {
+    switch (activeTabIndex) {
+      case 0:
+        return <p>계정정보 탭 내용</p>;
+      case 1:
+        return <p>계좌내역 탭 내용</p>;
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <main>
+      <MyPageSectionTabNav
+        activeIndex={activeTabIndex}
+        onTabClick={handleTabClick}
+      />
+      <section className='mt-6'>{renderTabContent()}</section>
+    </main>
+  );
 };
 
-export default AboutPage;
+export default MyPage;
