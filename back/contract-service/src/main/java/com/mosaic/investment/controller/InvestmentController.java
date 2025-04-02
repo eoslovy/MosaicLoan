1aabf17d014bb4243bbf3d7334995974ec2e9f90
recreate.kang <recreate.kang@gmail.com>
2025-04-02 13:43:02 +0900

    feat: TODO 서비스 흐름도 작성
    Former-commit-id: 3fe648dfa6c0241af7f927e6c8b293762d7ee1bc

--- back/contract-service/src/main/java/com/mosaic/investment/controller/InvestmentController.java
+++ back/contract-service/src/main/java/com/mosaic/investment/controller/InvestmentController.java
@@ -15,9 +15,9 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("investment")
 public class InvestmentController {
 
-	//TODO 돈빌리기
 	InvestmentService investmentService;
 
+	//TODO 투자하기
 	@PostMapping("")
 	public String startInvestment(StartInvestRequestDto requestDto) {
 		investmentService.createInvestment(requestDto);
@@ -29,4 +29,15 @@ public class InvestmentController {
 	public String getTotalBalance() {
 		return null;
 	}
+
+
+	
+	//TODO 종료하고 계좌로 환급하기
+	//TODO 돈 환급(스케쥴러)
+	//scheduele: 돈 환급 계약 만료
+	//approve: 계약이 완료되었는지 확인
+	//error: 미상환 계약 여부 확인 및 자동 이전처리
+	//pub: 계좌기준으로 kafka 이벤트 발행
+	//sub: 계좌에 돈 입금 처리 완료 확인
+	//approve: 계약 status 종료
 }
--- back/contract-service/src/main/java/com/mosaic/loan/controller/LoanController.java
+++ back/contract-service/src/main/java/com/mosaic/loan/controller/LoanController.java
@@ -15,10 +15,25 @@ import lombok.RequiredArgsConstructor;
 public class LoanController {
 
 	LoanService loanService;
-	//TODO 돈빌리기
+	//TODO 돈 빌리기
+	//api : 신용평가 api
+	//pub : 대출신청 발행
+	//sub : 빌려줄 돈 모금 로직
+	//approve : 모금이 완료되면 검증
+	//append: 데이터베이스에 해당 사항 반영
+	//pub : 대출 모금 금액 계좌로 인출
+	//status: 대출 상태 실행으로 변경
+	//sub : 인출 확인 트랜젝션
 	@PostMapping("")
 	public String createLoan(CreateLoanRequestDto request) {
 		loanService.createLoan(request);
 		return null;
 	}
+
+	//TODO 돈 갚기(스케쥴러)
+	//scheduele: 돈 갚는 계약
+	//pub: 계좌기준으로 kafka 이벤트 발행
+	//sub: 계좌에 돈 입금 확인
+	//transaction: 이자 및 금액 분배
+
 }
