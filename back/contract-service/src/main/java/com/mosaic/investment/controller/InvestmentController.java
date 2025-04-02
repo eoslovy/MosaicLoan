418a81d814c497f82e67061cc09ec5828296f5cc
recreate.kang <recreate.kang@gmail.com>
2025-04-02 19:38:18 +0900

    fix: import bigdecimal
    Former-commit-id: 5909efe55f3c9c3ce27a5cabb5326ca8494afc80

--- back/contract-service/src/main/java/com/mosaic/investment/controller/InvestmentController.java
+++ back/contract-service/src/main/java/com/mosaic/investment/controller/InvestmentController.java
@@ -48,6 +48,7 @@ public class InvestmentController {
 	//approve: 계약이 완료되었는지 확인
 	//error: 미상환 계약 여부 확인 및 자동 이전처리
 	//pub: 계좌기준으로 kafka 이벤트 발행
+
 	//sub: 계좌에 돈 입금 처리 완료 확인
 	//approve: 계약 status 종료
 }
