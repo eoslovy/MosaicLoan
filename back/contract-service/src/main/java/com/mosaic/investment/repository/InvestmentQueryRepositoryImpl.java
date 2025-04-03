4a51500a0cf6d0da22b3a07a0271e2e65fbcfdf5
recreate.kang <recreate.kang@gmail.com>
2025-04-04 02:36:51 +0900

    fix: querydsl config작성
    Former-commit-id: e98c3c0f32dfb534e25f533ba73c2d65d60e9877

--- back/contract-service/src/main/java/com/mosaic/investment/repository/InvestmentQueryRepositoryImpl.java
+++ back/contract-service/src/main/java/com/mosaic/investment/repository/InvestmentQueryRepositoryImpl.java
@@ -1,6 +1,17 @@
 package com.mosaic.investment.repository;
 
-/*
+
+import com.mosaic.core.model.Investment;
+import com.mosaic.core.model.QInvestment;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 @RequiredArgsConstructor
 public class InvestmentQueryRepositoryImpl implements InvestmentQueryRepository {
@@ -10,22 +21,26 @@ public class InvestmentQueryRepositoryImpl implements InvestmentQueryRepository 
     @Override
     public List<Investment> findQualifiedInvestments(BigDecimal minimumAmount, LocalDate loanDueDate) {
         QInvestment investment = QInvestment.investment;
+        BigDecimal minAmount = BigDecimal.valueOf(100000);
 
-        // 기준금액 = MAX(최소기준금액, 본인 원금 * 1/500)
-        NumberExpression<BigDecimal> thresholdAmount =
-                investment.principal.multiply(BigDecimal.valueOf(1.0 / 500)).max(minimumAmount);
+        BooleanExpression amountCondition = investment.amount.goe(minimumAmount)
+                .or(investment.amount.goe(
+                        investment.principal.multiply(1.0 / 500)
+                ));
 
-        BooleanExpression condition = investment.amount.goe(thresholdAmount)
-                .and(investment.targetRate.gt(investment.currentRate))
-                .and(investment.dueDate.loe(loanDueDate));
+        BooleanExpression rateCondition = investment.targetRate.gt(investment.currentRate);
+        BooleanExpression dueDateCondition = investment.dueDate.loe(loanDueDate);
+
+        List<Investment> result = queryFactory
+                .selectFrom(investment)
+                .where(amountCondition, rateCondition, dueDateCondition)
+                .orderBy(investment.amount.desc())
+                .fetch();
 
         return queryFactory
                 .selectFrom(investment)
-                .where(condition)
+                .where(amountCondition)
                 .orderBy(investment.amount.desc())
                 .fetch();
     }
-
-
 }
-*/
