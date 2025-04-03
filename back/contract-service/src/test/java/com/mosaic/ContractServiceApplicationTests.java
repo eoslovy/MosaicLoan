f57a11b7b7b4cff02f73749b62f25ef45c92d9d0
recreate.kang <recreate.kang@gmail.com>
2025-04-04 02:09:40 +0900

    fix: test 주석 제거
    Former-commit-id: afdf37f7a2175871f2f6068475cf103fceb14cca

--- back/contract-service/src/test/java/com/mosaic/ContractServiceApplicationTests.java
+++ back/contract-service/src/test/java/com/mosaic/ContractServiceApplicationTests.java
@@ -1,6 +1,9 @@
 package com.mosaic;
 
-/*
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
 @SpringBootTest
 class ContractServiceApplicationTests {
 
@@ -9,4 +12,3 @@ class ContractServiceApplicationTests {
     }
 
 }
-*/
