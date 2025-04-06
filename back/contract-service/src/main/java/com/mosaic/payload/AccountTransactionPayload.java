44afe8490046f4bc29bd1683242b02681426e7ef
recreate.kang <recreate.kang@gmail.com>
2025-04-06 17:22:05 +0900

    feat: investment 출금 payload 빌더 추가
    Former-commit-id: ef7118da3d0c6ad316724827741fafbd24ba564d

--- back/contract-service/src/main/java/com/mosaic/payload/AccountTransactionPayload.java
+++ back/contract-service/src/main/java/com/mosaic/payload/AccountTransactionPayload.java
@@ -1,10 +1,10 @@
 package com.mosaic.payload;
 
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-
 import com.mosaic.core.model.Investment;
 import com.mosaic.investment.dto.RequestInvestmentDto;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 public record AccountTransactionPayload(Integer accountId, Integer targetId, BigDecimal amount,
 										LocalDateTime createdAt) {
@@ -14,6 +14,14 @@ public record AccountTransactionPayload(Integer accountId, Integer targetId, Big
 			investment.getId(),
 			requestDto.principal(),
 			investment.getCreatedAt()
+		);
+	}
+	public static AccountTransactionPayload buildInvestWithdrawal(Investment investment, BigDecimal amountWithdraw) {
+		return new AccountTransactionPayload(
+				investment.getAccountId(),
+				investment.getId(),
+				amountWithdraw,
+				investment.getCreatedAt()
 		);
 	}
 }
