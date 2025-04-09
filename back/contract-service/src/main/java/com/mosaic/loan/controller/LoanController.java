678e66ed3d7e6d5eef859bfe186ed6e876d38a6d
오지석 <jiseok95@gmail.com>
2025-04-09 10:57:27 +0900

    fix: conflict 해결 과정에서 누락된 import 수정
    Former-commit-id: a17da566e9dca926c6e6cce987a00e92e823f293

--- back/contract-service/src/main/java/com/mosaic/loan/controller/LoanController.java
+++ back/contract-service/src/main/java/com/mosaic/loan/controller/LoanController.java
@@ -5,19 +5,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.mosaic.investment.dto.RequestInvestmentDto;
 import com.mosaic.loan.dto.CreateLoanRequestDto;
-import com.mosaic.loan.service.LoanService;
-import com.mosaic.loan.dto.LoanTransactionSearchRequest;
-import com.mosaic.loan.dto.LoanTransactionResponse;
-import com.mosaic.loan.repository.LoanQueryRepository;
-import com.mosaic.loan.dto.LoanTransactionsResponse;
 import com.mosaic.loan.dto.LoanOverviewResponse;
+import com.mosaic.loan.dto.LoanTransactionResponse;
+import com.mosaic.loan.dto.LoanTransactionSearchRequest;
+import com.mosaic.loan.dto.LoanTransactionsResponse;
+import com.mosaic.loan.dto.RepayLoanDto;
+import com.mosaic.loan.repository.LoanQueryRepository;
+import com.mosaic.loan.service.LoanService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -26,8 +26,8 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("loans")
 public class LoanController {
 
-    private final LoanService loanService;
-    private final LoanQueryRepository loanQueryRepository;
+	private final LoanService loanService;
+	private final LoanQueryRepository loanQueryRepository;
 
 	//TODO 돈 빌리기
 	@PostMapping("request/test")
@@ -45,24 +45,24 @@ public class LoanController {
 		return ResponseEntity.accepted().build();
 	}
 
-    //TODO 내 대출내역 확인
-    @GetMapping("overview")
-    public ResponseEntity<LoanOverviewResponse> getLoanOverview(@RequestHeader("X-MEMBER-ID") Integer memberId) {
-        return ResponseEntity.ok(loanQueryRepository.getLoanOverview(memberId));
-    }
+	//TODO 내 대출내역 확인
+	@GetMapping("overview")
+	public ResponseEntity<LoanOverviewResponse> getLoanOverview(@RequestHeader("X-MEMBER-ID") Integer memberId) {
+		return ResponseEntity.ok(loanQueryRepository.getLoanOverview(memberId));
+	}
 
-    //TODO 내 개별 투자의 거래내역 확인
-    // 대출 거래 내역 조회
-    @GetMapping("/transactions/search")
-    public ResponseEntity<LoanTransactionResponse> getLoanTransactions(
-        @RequestBody LoanTransactionSearchRequest request,
-        @RequestHeader("X-MEMBER-ID") Integer memberId) {
-        return ResponseEntity.ok(loanQueryRepository.searchTransactions(request, memberId));
-    }
+	//TODO 내 개별 투자의 거래내역 확인
+	// 대출 거래 내역 조회
+	@GetMapping("/transactions/search")
+	public ResponseEntity<LoanTransactionResponse> getLoanTransactions(
+		@RequestBody LoanTransactionSearchRequest request,
+		@RequestHeader("X-MEMBER-ID") Integer memberId) {
+		return ResponseEntity.ok(loanQueryRepository.searchTransactions(request, memberId));
+	}
 
-    // 대출 단건 거래 내역 조회
-    @GetMapping("/{loan_id}")
-    public ResponseEntity<LoanTransactionsResponse> getLoanTransactions(@PathVariable("loan_id") Integer loanId) {
-        return ResponseEntity.ok(loanQueryRepository.findContractsByLoanId(loanId));
-    }
+	// 대출 단건 거래 내역 조회
+	@GetMapping("/{loan_id}")
+	public ResponseEntity<LoanTransactionsResponse> getLoanTransactions(@PathVariable("loan_id") Integer loanId) {
+		return ResponseEntity.ok(loanQueryRepository.findContractsByLoanId(loanId));
+	}
 }
