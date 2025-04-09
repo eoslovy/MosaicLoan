77f0e7fba125241844f0c412ccc708c5a1c5b49f
eoslovy <ydg0630@naver.com>
2025-04-09 12:43:32 +0900

    feat: statistics 스케줄링 및 호출 api 구현
    Former-commit-id: cf23cf48357ef000576b597f6d0a90e3869f3c27

--- back/contract-service/src/main/java/com/mosaic/ContractServiceApplication.java
+++ back/contract-service/src/main/java/com/mosaic/ContractServiceApplication.java
@@ -2,8 +2,10 @@ package com.mosaic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ContractServiceApplication {
 
     public static void main(String[] args) {
--- back/contract-service/src/main/java/com/mosaic/investment/repository/InvestmentRepository.java
+++ back/contract-service/src/main/java/com/mosaic/investment/repository/InvestmentRepository.java
@@ -1,10 +1,30 @@
 package com.mosaic.investment.repository;
 
-import com.mosaic.core.model.Investment;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import com.mosaic.core.model.Investment;
+import com.mosaic.statistics.repository.RateStatProjection;
 
 @Repository
 public interface InvestmentRepository extends JpaRepository<Investment, Integer> {
-
+	@Query(value = """
+    SELECT
+        target_rate AS targetRate,
+        FLOOR(current_rate / 100.0 * 10) / 10 AS actualRate,
+        COUNT(*) AS count
+    FROM mosaic_contract.investment
+    WHERE due_date < CURDATE()
+      AND target_rate BETWEEN 500 AND 1000
+      AND current_rate BETWEEN 500 AND 1000
+    GROUP BY 
+        target_rate,
+        FLOOR(current_rate / 100.0 * 10) / 10
+    ORDER BY 
+        target_rate, actualRate
+    """, nativeQuery = true)
+	List<RateStatProjection> findStatistics();
 }
