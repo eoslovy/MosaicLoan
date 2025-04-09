a91d92678c541088f0f8e4e455f65841aaee0e9b
eoslovy <ydg0630@naver.com>
2025-04-09 16:02:43 +0900

    feat: contract-service QueryDSL을 통한 조회 API 추가
    Former-commit-id: 00169b4d90b7fa70b3237c4602518ff67f519bc5

--- back/contract-service/src/main/java/com/mosaic/investment/repository/InvestmentQueryRepository.java
+++ back/contract-service/src/main/java/com/mosaic/investment/repository/InvestmentQueryRepository.java
@@ -9,6 +9,9 @@ import com.mosaic.core.model.Loan;
 import com.mosaic.investment.dto.InvestmentTransactionResponse;
 import com.mosaic.investment.dto.InvestmentTransactionSearchRequest;
 import com.mosaic.investment.dto.InvestmentOverviewDto;
+import com.mosaic.investment.dto.InvestmentSummaryResponse;
+import com.mosaic.investment.dto.InvestmentListResponse;
+import com.mosaic.investment.dto.ProfitHistoryResponse;
 
 public interface InvestmentQueryRepository {
     List<Investment> findQualifiedInvestments(BigDecimal minimumAmount, Loan loan);
@@ -18,4 +21,13 @@ public interface InvestmentQueryRepository {
     
     // 투자 거래 내역을 검색 조건에 따라 조회
     InvestmentTransactionResponse searchTransactions(InvestmentTransactionSearchRequest request);
+    
+    // 투자 요약 정보 조회 (계약 만기일이 현재 시간보다 앞인 투자만)
+    InvestmentSummaryResponse getInvestmentSummary(Integer memberId);
+    
+    // 최근 투자 목록 조회 (최신순 10개)
+    InvestmentListResponse getRecentInvestments(Integer memberId);
+    
+    // 수익 내역 조회 (만기일 지난 투자, 최대 10개)
+    ProfitHistoryResponse getProfitHistory(Integer memberId);
 }
