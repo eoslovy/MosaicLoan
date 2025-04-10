bd8cc9106706a577319693453ebf727cc960c54c
박유진 <galilee155@naver.com>
2025-04-10 13:05:04 +0900

    fix: rerendering page
    Former-commit-id: f99fb7a01b5bfe7b5eab8fd70b8fe5079fb150c7

--- front/src/app/investor/page.tsx
+++ front/src/app/investor/page.tsx
@@ -1,5 +1,20 @@
-import { redirect } from 'next/navigation';
+'use client';
 
-export default function InvestorPage() {
-  redirect('/investor/overview');
-}
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+const InvestorPage = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const redirectPath = '/investor/overview';
+
+    localStorage.setItem('lastVisited', redirectPath);
+
+    router.replace(redirectPath);
+  }, [router]);
+
+  return null;
+};
+
+export default InvestorPage;
